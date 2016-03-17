40a4f3ceccbded99bce3b521820dc2220a25440e
Justin Makaila <justinmakaila@gmail.com>
2016-03-17 12:25:22 -0400

    Initial Commit

---
+++ GraphQLMapping/GraphQLMapping.h
@@ -0,0 +1,19 @@
+//
+//  GraphQLMapping.h
+//  GraphQLMapping
+//
+//  Created by Justin Makaila on 3/17/16.
+//  Copyright © 2016 justinmakaila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GraphQLMapping.
+FOUNDATION_EXPORT double GraphQLMappingVersionNumber;
+
+//! Project version string for GraphQLMapping.
+FOUNDATION_EXPORT const unsigned char GraphQLMappingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GraphQLMapping/PublicHeader.h>
+
+
